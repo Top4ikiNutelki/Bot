a145ecf9f3655337b8ab80f21738ee4d770383c0
AlekseyVorobev7 <56201242+AlekseyVorobev7@users.noreply.github.com>
2020-02-20 21:15:17 +0300

    Add files via upload
    Нормальный код основной конструкции.Теперь пилим функции для бота

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import vk_api
+from datetime import datetime
+import time
 
-vk = vk_api.VkApi(token="66a97041b8f14ec84cf994d197ef4cba186a7e3d3563b1e17251dd25433a5d98c653661e241a7bd787d39")
 
+def write_msg(peer_id, message):
+    vk.method('messages.send', {'peer_id': peer_id, "random_id": 0, 'message': message})
+
+
+token = "66a97041b8f14ec84cf994d197ef4cba186a7e3d3563b1e17251dd25433a5d98c653661e241a7bd787d39"
+vk = vk_api.VkApi(token=token)
 vk._auth_token()
-
-vk.get_api()
-
-longpoll = VkBotLongPoll(vk, 192093581)
+vk_api = vk.get_api()
+longpooll = VkBotLongPoll(vk, 192093581)
 
 while True:
-    for event in longpoll.listen():
+    for event in longpooll.listen():
+        response = event.object.text.lower()
         if event.type == VkBotEventType.MESSAGE_NEW:
-            if event.object.peer_id != event.object.from_id:
-                if event.object.text.lower() == "привет":
-                    vk.method("messages.send", {"peer_id": event.object.peer_id, "message": event.object.text,
-                                                "random_id": 0})
-            elif event.object.peer_id == event.object.from_id:
-                if event.object.text.lower() == "привет":
-                    vk.method("messages.send", {"user_id": event.object.from_id, "message": event.object.text,
-                                                "random_id": 0})
-            if event.object.peer_id != event.object.from_id:
-                if event.object.text.lower() == "пока":
-                    vk.method("messages.send", {"peer_id": event.object.peer_id, "message": event.object.text,
-                                                        "random_id": 0})
-            elif event.object.peer_id == event.object.from_id:
-                if event.object.text.lower() == "пока":
-                    vk.method("messages.send", {"user_id": event.object.from_id, "message": event.object.text,
-                                                        "random_id": 0})
+            if response == 'привет бот':
+                write_msg(event.object.peer_id, "Привет =)")
+            elif response == 'пока':
+                write_msg(event.object.peer_id, "Пока")
+            elif response == '!хелп' or response == '/help':
+                write_msg(event.object.peer_id)
+
+
+
 
